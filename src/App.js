f757dd5007f1f778a6cac8778aa108503db249b8
Mayconn Oliveira <mayconnwgo@gmail.com>
2020-04-08 22:38:26 -0300

    Write the code to pass all the tests

--- src/App.js
+++ src/App.js
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [projects, setProjects] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const owner = "Mayconn";
+
+  useEffect(() => {
+    const load = async () => {
+      const { data } = await api.get("repositories");
+      setProjects(data);
+    };
+    load();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post("repositories", {
+      title: inputValue,
+      owner,
+    });
+
+    setProjects([...projects, data]);
+    setInputValue("");
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const foundProject = projects.find((item) => item.id === id);
+    if (foundProject) {
+      await api.delete(`repositories/${id}`);
+      setProjects(projects.filter((item) => item.id !== id));
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {projects.map(({ title, id }) => (
+          <li key={id}>
+            {title}
+            <button onClick={() => handleRemoveRepository(id)}>Remover</button>
+          </li>
+        ))}
       </ul>
 
+      <input
+        className="input"
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
